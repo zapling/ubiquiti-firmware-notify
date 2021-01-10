d67bcdb15de82bdb95676408cc69c13812652dac
Andreas Palm <andreaspalm98@gmail.com>
2021-01-10 20:31:23 +0100

    Basic API

--- ubiquiti/firmware.go
+++ ubiquiti/firmware.go
@@ -1,6 +1,9 @@
 package ubiquiti
 
 import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -11,6 +14,7 @@ type Firmware struct {
 	Featured        bool   `json:"featured"`
 	CategroySlug    string `json:"category__slug"`
 	SdkID           int    `json:"sdk__id"`
+	Id              int    `json:"id"`
 	Size            string `json:"size"`
 	Mib             string `json:"mib"`
 	Filename        string `json:"filename"`
@@ -29,5 +33,44 @@ type Firmware struct {
 	Architecture    string `json:"architecture"`
 }
 
-func GetAvailableFirmware(device string) {
+func GetAvailableFirmware(device string) ([]Firmware, error) {
+	url := "https://www.ui.com/download?group=" + device
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, errors.New("An error occurred when trying to create a new request")
+	}
+
+	req.Header.Add("X-Requested-With", "XMLHttpRequest") // Fool Ubiquiti API that we are a browser
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, errors.New("Error when sending request")
+	}
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, errors.New("Unable to read response body")
+	}
+
+	firmware, err := unmarshalFirmwareRequest(bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return firmware, nil
+}
+
+func unmarshalFirmwareRequest(body []byte) ([]Firmware, error) {
+	type firmwareResponse struct {
+		Downloads []Firmware
+	}
+
+	var response firmwareResponse
+
+	if err := json.Unmarshal(body, &response); err != nil {
+		return nil, errors.New("Unable to convert response to json")
+	}
+
+	return response.Downloads, nil
 }
