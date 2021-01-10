17f1eb19fc3185c4a97b00b27f1dc1f2f3d9765d
Andreas Palm <andreaspalm98@gmail.com>
2021-01-10 21:51:27 +0100

    Added EDGEMAX product line

---
+++ ubiquiti/device.go
@@ -0,0 +1,26 @@
+package ubiquiti
+
+const (
+	// Edge MAX
+	Edge_Point           = "edgepoint"
+	Edge_Router          = "edgerouter"
+	Edge_Router_10X      = "edgerouter-10x"
+	Edge_Router_12       = "edgerouter-12"
+	Edge_Router_12P      = "edgerouter-12p"
+	Edge_Router_4        = "edgerouter-4"
+	Edge_Router_6P       = "edgerouter-6p"
+	Edge_Router_Infinity = "edgerouter-infinity"
+	Edge_Router_Lite     = "edgerouter-lite"
+	Edge_Router_PoE      = "edgerouter-poe"
+	Edge_Router_Pro      = "edgerouter-pro"
+	Edge_Router_X        = "edgerouter-x"
+	Edge_Router_X_SFP    = "edgerouter-x-sfp"
+	Edge_Switch          = "edgeswitch"
+	Edge_Switch_10X      = "edgeswitch-10x"
+	Edge_Switch_10XP     = "edgeswitch-10xp"
+	Edge_Switch_12F      = "edgeswitch-12f"
+	Edge_Switch_16XG     = "edgeswitch-16-xg"
+	Edge_Switch_8_150w   = "edgeswitch-8-150w"
+	Edge_Switch_Lite     = "edgeswitch-lite"
+	Edge_Switch_XP       = "edgeswitch-xp"
+)
