b98da6b4ad4219d750ac3278bc8f65f92dc2f3cd
Andreas Palm <andreaspalm98@gmail.com>
2021-01-10 20:32:06 +0100

    Basic usage

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"github.com/zapling/ubiquiti-firmware-notify/ubiquiti"
 )
 
 func main() {
-    fmt.Println("Hello World")
+	firmwares, err := ubiquiti.GetAvailableFirmware("edgerouter-x-sfp")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var items []ubiquiti.Firmware
+	for _, firm := range firmwares {
+		if firm.CategroySlug == "firmware" {
+			items = append(items, firm)
+		}
+	}
+
+	for _, item := range items {
+		fmt.Println(item.DatePublished, item.Name)
+	}
+
+	// fmt.Printf("%+v", items[0])
 }
